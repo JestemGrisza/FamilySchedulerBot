43f0f930e025d1e1072cb793b838fd9af6009377
JestemGrisza <alexander.a.grigoriev@gmail.com>
2021-07-07 11:58:56 +0300

    Family Scheduler. Beetroot project.

--- bot.sh
+++ bot.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+#Use "sudo docker exec -it bot /bin/bash" to connect to container.
+#Use "sudo docker attach bot" for attach terminal.
 cur_dir=$(exec pwd)
 echo "$cur_dir"
 case "$1" in
@@ -17,15 +19,15 @@ case "$1" in
             sudo docker stop bot
             echo "."
            ;;
-   start)
+    start)
             echo "Start bot"
             sudo docker stop bot
             sudo docker run --name bot -d --rm -v "$cur_dir":/usr/src/fml_schd/db fml_schd
             sudo docker ps
             echo "."
             ;;
-        *)
+    *)
           echo "Usage: bot.sh install|stop|start"
           exit 1
           ;;
-    esac
+esac
